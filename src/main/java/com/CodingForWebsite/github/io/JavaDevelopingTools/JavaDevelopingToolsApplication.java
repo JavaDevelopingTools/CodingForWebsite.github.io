c8e48fac8b76e61f63eaffea898a2d81bec7b595
Allu <javadevelopingtools@gmail.com>
2019-05-19 22:04:38 -0700

    initial
    outputs hello world

---
+++ src/main/java/com/CodingForWebsite/github/io/JavaDevelopingTools/JavaDevelopingToolsApplication.java
@@ -0,0 +1,13 @@
+package com.CodingForWebsite.github.io.JavaDevelopingTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevelopingToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevelopingToolsApplication.class, args);
+		System.out.println("hello world");
+	}
+}
---
+++ src/test/java/com/CodingForWebsite/github/io/JavaDevelopingTools/JavaDevelopingToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.CodingForWebsite.github.io.JavaDevelopingTools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDevelopingToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
